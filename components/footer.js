28a5a041dc5d34b27836b6e5dfddeb46618efb08
HamzaQahoush <hamza.qah@gmail.com>
2021-08-03 20:34:20 +0300

    finish lab38

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+export default function Footer(props){
+    return(
+        <footer className="flex justify-between p-4 bg-green-500">
+
+        <h2>{props.numberoflocaton} Location World Wide</h2>
+
+        </footer>
+    );
+  }
